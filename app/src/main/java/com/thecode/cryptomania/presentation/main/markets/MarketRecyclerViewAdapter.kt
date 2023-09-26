ec85abb022ae48518e9dec87258f3bb379f2d89d
Gabriel TEKOMBO <tekombo.gabriel@gmail.com>
2023-09-26 15:44:18 +0200

    clean up code

--- app/src/main/java/com/thecode/cryptomania/presentation/main/markets/MarketRecyclerViewAdapter.kt
+++ app/src/main/java/com/thecode/cryptomania/presentation/main/markets/MarketRecyclerViewAdapter.kt
@@ -1,6 +1,5 @@
 package com.thecode.cryptomania.presentation.main.markets
 
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -18,79 +17,75 @@ import com.thecode.cryptomania.utils.extensions.addPrefix
 import com.thecode.cryptomania.utils.extensions.addSuffix
 import com.thecode.cryptomania.utils.extensions.withNumberSuffix
 
-
 class MarketRecyclerViewAdapter(private val listener: CoinCardOnClickListener) :
-        RecyclerView.Adapter<MarketRecyclerViewAdapter.CoinViewHolder>() {
+	RecyclerView.Adapter<MarketRecyclerViewAdapter.CoinViewHolder>() {
+	private lateinit var binding: AdapterMarketCryptoBinding
+	private var coinsList: List<CoinItem> = listOf()
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinViewHolder {
+		binding = AdapterMarketCryptoBinding.inflate(
+			LayoutInflater.from(parent.context),
+			parent,
+			false
+		)
+		return CoinViewHolder(binding)
+	}
 
-    private lateinit var binding: AdapterMarketCryptoBinding
-    var coinsList: List<CoinItem> = listOf()
+	override fun getItemCount(): Int {
+		return coinsList.size
+	}
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinViewHolder {
-        binding = AdapterMarketCryptoBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-        )
-        return CoinViewHolder(binding)
-    }
+	override fun onBindViewHolder(holder: CoinViewHolder, position: Int) {
+		val coin = coinsList[position]
+		holder.apply {
+			tvCoinName.text = coin.symbol
+			tvMarketCap.text = coin.market_cap.withNumberSuffix().addPrefix("$")
+			if (coin.price_change_percentage_24h > 0) {
+				tvCoinPrice.setTextColor(
+					ContextCompat.getColor(
+						holder.container.context,
+						R.color.md_green_400
+					)
+				)
+				layoutPercentage.setBackgroundResource(R.drawable.rounded_background_green)
+			} else {
+				layoutPercentage.setBackgroundResource(R.drawable.rounded_background_red)
+				tvCoinPrice.setTextColor(
+					ContextCompat.getColor(
+						holder.container.context,
+						R.color.md_red_400
+					)
+				)
+			}
+			tvCoinPrice.text = coin.current_price.toString().addPrefix("$")
+			val percent = String.format("%.2f", coin.price_change_percentage_24h)
+			tvCoinPercentage.text = percent.addSuffix("%")
 
-    override fun getItemCount(): Int {
-        return coinsList.size
-    }
+			Glide.with(holder.itemView.context).load(coin.image)
+				.placeholder(R.drawable.ic_baseline_monetization_on_gray_24)
+				.error(R.drawable.ic_baseline_monetization_on_gray_24)
+				.apply(RequestOptions().centerCrop())
+				.into(image)
 
-    override fun onBindViewHolder(holder: CoinViewHolder, position: Int) {
-        val coin = coinsList[position]
-        holder.tvCoinName.text = coin.symbol
-        holder.tvMarketCap.text = coin.market_cap.withNumberSuffix().addPrefix("$")
-        if (coin.price_change_percentage_24h > 0) {
-            holder.tvCoinPrice.setTextColor(
-                    ContextCompat.getColor(
-                            holder.container.context,
-                            R.color.md_green_400
-                    )
-            )
-            holder.layoutPercentage.setBackgroundResource(R.drawable.rounded_background_green)
+			container.setOnClickListener {
+				listener.openCoinDetails(coin)
+			}
+		}
+	}
 
-        } else {
-            holder.layoutPercentage.setBackgroundResource(R.drawable.rounded_background_red)
-            holder.tvCoinPrice.setTextColor(
-                    ContextCompat.getColor(
-                            holder.container.context,
-                            R.color.md_red_400
-                    )
-            )
-        }
-        holder.tvCoinPrice.text = coin.current_price.toString().addPrefix("$")
-        val percent = String.format("%.2f", coin.price_change_percentage_24h)
-        holder.tvCoinPercentage.text = percent.addSuffix("%")
+	fun setCoinListItems(coinsList: ArrayList<CoinItem>) {
+		this.coinsList = emptyList()
+		this.coinsList = coinsList
+		notifyDataSetChanged()
+	}
 
-        Glide.with(holder.itemView.context).load(coin.image)
-                .placeholder(R.drawable.ic_baseline_monetization_on_gray_24)
-                .error(R.drawable.ic_baseline_monetization_on_gray_24)
-                .apply(RequestOptions().centerCrop())
-                .into(holder.image)
-
-        holder.container.setOnClickListener {
-            listener.openCoinDetails(coin)
-        }
-    }
-
-    fun setCoinListItems(coinsList: ArrayList<CoinItem>) {
-        this.coinsList = emptyList()
-        this.coinsList = coinsList
-        notifyDataSetChanged()
-    }
-
-
-    class CoinViewHolder(binding: AdapterMarketCryptoBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-
-        val container: RelativeLayout = binding.layoutContainer
-        val tvCoinName: TextView = binding.textName
-        val tvMarketCap: TextView = binding.textCap
-        val tvCoinPrice: TextView = binding.textPrice
-        val layoutPercentage: RelativeLayout = binding.layoutPercent
-        val tvCoinPercentage: TextView = binding.textPercentage
-        val image: ImageView = binding.imgIcon
-    }
+	class CoinViewHolder(binding: AdapterMarketCryptoBinding) :
+		RecyclerView.ViewHolder(binding.root) {
+		val container: RelativeLayout = binding.layoutContainer
+		val tvCoinName: TextView = binding.textName
+		val tvMarketCap: TextView = binding.textCap
+		val tvCoinPrice: TextView = binding.textPrice
+		val layoutPercentage: RelativeLayout = binding.layoutPercent
+		val tvCoinPercentage: TextView = binding.textPercentage
+		val image: ImageView = binding.imgIcon
+	}
 }
